7f4a323e25952a221395467468c66a36a36b6727
Anthony Thorpe <athorpe624@gmail.com>
2024-03-26 00:59:28 -0400

    .

--- scraper/scraper.go
+++ scraper/scraper.go
@@ -2,11 +2,13 @@ package scraper
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"os"
 	"strconv"
 	"sync"
 
+	"github.com/chromedp/cdproto/cdp"
 	"github.com/chromedp/chromedp"
 )
 
@@ -109,10 +111,61 @@ func Scrape(url string) (map[brand][]Vehicle, error) {
 	return ret, nil
 }
 
+const (
+	URL      = "https://www.rumbleon.com/buy?page=1"
+	URL_TEST = "https://scrapingclub.com/exercise/list_infinite_scroll/"
+	// USER_AGENT     = `Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36`
+	ENDING_ELEMENT = `#Layer_1-2 > path:nth-child(3)`
+)
+
 // TODO
 // grabs vehicles from the url
 func scrapeInventory(url string, opts []chromedp.ExecAllocatorOption) ([]Vehicle, error) {
 	ret := []Vehicle{}
+
+	allocCtx, cancel := chromedp.NewExecAllocator(context.Background(), opts...)
+	defer cancel()
+
+	taskCtx, cancel := chromedp.NewContext(allocCtx)
+	defer cancel()
+
+	var nodes []*cdp.Node
+
+	// <div class="ant-card css-1gl0vip-emotion--Result--cardCss ant-card-bordered ant-card-hoverable">
+	err := chromedp.Run(taskCtx,
+		chromedp.Navigate(url),
+		chromedp.WaitVisible(ENDING_ELEMENT),
+		chromedp.Nodes(".ant-card-body", &nodes, chromedp.ByQueryAll),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// brand:
+	// div.ant-card-body > div > div:nth-child(2) > span
+
+	// model:
+	// div.ant-card-body > div > div:nth-child(3) > span
+
+	// price:
+	// div:nth-child(1) > span > strong
+
+	var brand, model, price string
+	for _, node := range nodes {
+		err = chromedp.Run(taskCtx,
+			chromedp.Text(`div.ant-card-body > div > div:nth-child(2) > span`, &brand, chromedp.ByQuery, chromedp.FromNode(node)),
+			chromedp.Text(`div.ant-card-body > div > div:nth-child(3) > span`, &model, chromedp.ByQuery, chromedp.FromNode(node)),
+			chromedp.Text(`span.ant-typography > strong`, &price, chromedp.ByQuery, chromedp.FromNode(node)),
+		)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println(brand)
+		fmt.Println(model)
+		fmt.Println(price)
+		fmt.Println()
+	}
 
 	return ret, nil
 }
--- scraper/scraper_test.go
+++ scraper/scraper_test.go
@@ -54,27 +54,21 @@ func TestMaxPage(t *testing.T) {
 
 }
 
-const (
-	URL      = "https://www.rumbleon.com/buy?page=1"
-	URL_TEST = "https://scrapingclub.com/exercise/list_infinite_scroll/"
-	// USER_AGENT     = `Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36`
-	ENDING_ELEMENT = `#Layer_1-2 > path:nth-child(3)`
-)
-
 func TestExample(t *testing.T) {
 	opts := []chromedp.ExecAllocatorOption{
 		chromedp.UserAgent(USER_AGENT),
+		chromedp.Headless,
 	}
 
-	ctx, cancel := chromedp.NewExecAllocator(context.Background(), opts...)
+	allocCtx, cancel := chromedp.NewExecAllocator(context.Background(), opts...)
 	defer cancel()
-	ctx, cancel = chromedp.NewContext(ctx)
+	taskCtx, cancel := chromedp.NewContext(allocCtx)
 	defer cancel()
 
 	var nodes []*cdp.Node
 
 	// <div class="ant-card css-1gl0vip-emotion--Result--cardCss ant-card-bordered ant-card-hoverable">
-	err := chromedp.Run(ctx,
+	err := chromedp.Run(taskCtx,
 		chromedp.Navigate(URL),
 		chromedp.WaitVisible(ENDING_ELEMENT),
 		chromedp.Nodes(".ant-card-body", &nodes, chromedp.ByQueryAll),
@@ -94,7 +88,7 @@ func TestExample(t *testing.T) {
 
 	var brand, model, price string
 	for _, node := range nodes {
-		err = chromedp.Run(ctx,
+		err = chromedp.Run(taskCtx,
 			chromedp.Text(`div.ant-card-body > div > div:nth-child(2) > span`, &brand, chromedp.ByQuery, chromedp.FromNode(node)),
 			chromedp.Text(`div.ant-card-body > div > div:nth-child(3) > span`, &model, chromedp.ByQuery, chromedp.FromNode(node)),
 			chromedp.Text(`span.ant-typography > strong`, &price, chromedp.ByQuery, chromedp.FromNode(node)),
